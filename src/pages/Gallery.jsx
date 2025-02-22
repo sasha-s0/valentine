ad61af800046770f26791fb4693700fc07219ef3
vovvkka <v.golem2288@gmail.com>
2025-02-22 17:58:18 +0300

    media fix

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import {useNavigate} from "react-router-dom";
 
 const images = [
-    "../assets/gallery/0.jpg",
+    "../public/assets/gallery/0.jpg",
     "../assets/gallery/1.jpg",
     "../assets/gallery/2.jpg",
     "../assets/gallery/3.jpg",
